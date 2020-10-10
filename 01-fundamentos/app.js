37292afccaf965764056104376bee840b5524f69
carlosGuzman <carlos.guzman.0515@gmail.com>
2020-10-10 09:03:27 -0500

    Trabajando con la consola

--- 01-fundamentos/app.js
+++ 01-fundamentos/app.js
@@ -9,6 +9,25 @@
 
 let a = 10, b = 10, c = 10, d = 10, x = a + b;
 
+let saludos = 'Hola ',
+    superHero1 = "Spiderman"
+
+const saludosHeroe = saludos + superHero1
+
 console.log(x)
-console.war(x)
+console.warn(x)
 console.error(x)
+console.info(x)
+
+console.log('a', a)
+console.log('b', b)
+console.log('c', c)
+
+console.log({a})
+console.log({b})
+console.log({c})
+
+//dar css en la consola
+console.log('%c Mis variables', 'color:blue;font-weight:bold')
+
+console.table({a, b, c, d, x,  saludos, superHero1})
