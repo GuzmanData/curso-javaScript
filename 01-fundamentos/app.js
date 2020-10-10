def7cf8aea013f6d90c0ec9e7ef735f3e817079e
carlosGuzman <carlos.guzman.0515@gmail.com>
2020-10-10 08:47:22 -0500

    Iniciamos el curso

---
+++ 01-fundamentos/app.js
@@ -0,0 +1,14 @@
+// console.log('Hola mundo');
+
+// Variables
+// let a = 10;
+// var b = 10; // no recomendado usr el var, forma antigua
+// const c = 10;
+// c = 20; no se pueda hacer
+
+
+let a = 10, b = 10, c = 10, d = 10, x = a + b;
+
+console.log(x)
+console.war(x)
+console.error(x)
